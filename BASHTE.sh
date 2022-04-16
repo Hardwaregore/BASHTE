4cdbadf641b33a152e8868c3eda5abb70ad64708
Hardwaregore <hardwaregore101@gmail.com>
2022-04-16 12:20:42 -0500

    a

--- BASHTE.sh
+++ BASHTE.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 clear
-echo "################################################################"
-echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write. #"
-echo "#  :wq = Write and quit  :q! = quit and discard                #"
-echo "################################################################"
+echo "###############################################################################"
+echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write.               #"
+echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous line   #"
+echo "###############################################################################"
 echo ""
 read -p "Enter file name: " file
 touch $file
-touch tempfile.txt
+touch .BASHTE/MASTER/BASHTE
 clear
-echo "################################################################"
-echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write. #"
-echo "#  :wq = Write and quit  :q! = quit and discard                #"
-echo "################################################################"
-
+echo "###############################################################################"
+echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write.               #"
+echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous line   #"
+echo "###############################################################################"
 
 
 
 while true
-do  lines=$(wc -l tempfile.txt | cut -d " " -f 1)
+do  lines=$(wc -l .BASHTE/MASTER/BASHTE)
     read -p "$lines >" store
     if [ "$store" = "\\:q" ]
     then
         break
     elif [ "$store" = "\\:w" ]
     then
-        cat tempfile.txt >> $file
+        cat .BASHTE/* >> $file
     elif [ "$store" = "\\:wq" ]
     then
         cat tempfile.txt >> $file
@@ -46,10 +45,7 @@ done
 
 
 
-
-
-
-
+# | cut -d " " -f 1
 
 
 
