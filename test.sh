dc0bad50dbb850ecef19e3b9c45c178c3552e6d6
Hardwaregore <hardwaregore101@gmail.com>
2022-04-21 14:48:27 -0500

    Testing testing 123

--- test.sh
+++ test.sh
@@ -3,4 +3,7 @@
 echo $1 | wc $1 >> $secondarg
 
 if [ $secondarg -lt 2 ]; then
-echo "
+echo "You entered a word that has a word count of <2"
+else
+echo "You entered a word that has a word count of >2"
+fi
